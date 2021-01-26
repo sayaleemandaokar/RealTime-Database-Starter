089733abeb233e6ea3a099d56976893c4d748950
sayaleemandaokar <58383890+sayaleemandaokar@users.noreply.github.com>
2021-01-26 17:17:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+//TODO: DONE: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyBz0XCoMwVikrX5YK01XoOo3HP_DYH4nAk",
+  authDomain: "mygitapp-70be0.firebaseapp.com",
+  databaseURL: "https://mygitapp-70be0-default-rtdb.firebaseio.com",
+  projectId: "mygitapp-70be0",
+  storageBucket: "mygitapp-70be0.appspot.com",
+  messagingSenderId: "346636933444",
+  appId: "1:346636933444:web:7e9f0b4f060252836e9f11",
+  measurementId: "G-TH5Q3CM2MG"
+};
+
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
